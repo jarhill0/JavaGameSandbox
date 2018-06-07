733d8408edba5e58890b72fffdc30b94cec7895e
jarhill0 <jarhill0@users.noreply.github.com>
2018-06-07 10:28:29 -0700

    LOWER

--- src/Game.java
+++ src/Game.java
@@ -76,7 +76,7 @@ class Game extends JPanel {
         g.fillRect(0, 0, this.getWidth(), this.getHeight()); // fill the screen with the background color
         if (isPaused()) {
             g.setColor(Color.BLACK);
-            g.drawString("[ESC} to unpause\n[SPACE] to restart", getWidth() / 2 - 80, 0);
+            g.drawString("[ESC} to unpause\n[SPACE] to restart", getWidth() / 2 - 80, 150);
         }
 
         for (Paintable paintable : sprites)
